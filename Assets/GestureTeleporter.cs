b5a06bca0b297c0c09859be517f9f4d3964557c1
Marius Stubberud <mars@makingview.no>
2020-06-10 13:59:59 +0200

    Added new gesture for aiming

---
+++ Assets/GestureTeleporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OVRSkeleton))]
+public class GestureTeleporter : MonoBehaviour
+{
+    public GestureDetector gestureDetector;
+
+    private OVRSkeleton skeleton;
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        skeleton = GetComponent<OVRSkeleton>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (gestureDetector.IsGestureActive(PoseName.JazzHand))
+        {
+            Vector3 handDirection = skeleton.GetSkeletonType() == OVRSkeleton.SkeletonType.HandRight ? -transform.up : transform.up;
+            Vector3 start = transform.position;
+            Vector3 end = start + handDirection;
+            
+            lineRenderer.enabled = true;
+            lineRenderer.SetPositions(new List<Vector3>() { start, end }.ToArray());
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+}
