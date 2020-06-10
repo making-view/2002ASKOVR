dbb6ac0f83d28b8bb5b62b94d7b7c4e2d8936836
Marius Stubberud <mars@makingview.no>
2020-06-10 16:19:38 +0200

    Added basic calculation for teleporting

---
+++ Assets/Scripts/GestureTeleporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OVRSkeleton))]
+public class GestureTeleporter : MonoBehaviour
+{
+    public GestureDetector gestureDetector;
+    public float rayLength = 10f;
+
+    private OVRSkeleton skeleton;
+
+    void Start()
+    {
+        skeleton = GetComponent<OVRSkeleton>();
+    }
+
+    void Update()
+    {
+        if (gestureDetector.IsGestureActive(PoseName.JazzHand))
+        {
+            Vector3 palmUp = skeleton.GetSkeletonType() == OVRSkeleton.SkeletonType.HandRight ? -transform.right : transform.right;
+            Vector3 rayPointDirection = skeleton.GetSkeletonType() == OVRSkeleton.SkeletonType.HandRight ? -transform.up : transform.up;
+            Vector3 start = transform.position + palmUp * 0.08f;
+            Vector3 end = start + (rayPointDirection * rayLength) + (palmUp * (rayLength / 2));
+        }
+    }
+}
