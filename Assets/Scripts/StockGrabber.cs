af04f5d484fd34bd346fac0f8b961d13a3362f6c
Marius Stubberud <mars@makingview.com>
2020-06-24 12:26:49 +0200

    Made it impossible to teleport into shelves and truck. Also started basic grabbing setup.

--- Assets/Scripts/StockGrabber.cs
+++ Assets/Scripts/StockGrabber.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class StockGrabber : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GestureDetector gestureDetector;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
