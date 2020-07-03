a9a3e0893172dcf0fa90bbc52cd1d08504edeb5d
Marius Stubberud <mars@makingview.com>
2020-07-03 18:16:30 +0200

    Fixed gap with my genius big boy brain

--- Assets/Scripts/StockHolderBender.cs
+++ Assets/Scripts/StockHolderBender.cs
@@ -25,10 +25,11 @@ public class StockHolderBender : MonoBehaviour
         for (int part = 0; part < bendyParts.Length; ++part)
         {
             //
-            // +1 because zero-based index doesn't make sense for percentage calculation
-            // +1 more because first part isn't going to be bent programmatically
+            // Remaps from the range 1 to partCount to the range 0 to 1 to calculate percent
+            // Part +1 to bring minimum to 1
+            // Part +1 to skip first part
             //
-            var percent = (part + 2f) / partCount;
+            var percent = RemapRange((part + 2f), 1f, partCount, 0, 1);
 
             bendyParts[part].transform.position = GetPointOnBezierCurve(p0, p1, p2, percent);
             bendyParts[part].transform.rotation = Quaternion.Lerp(firstRotation, lastRotation, percent);
@@ -44,5 +45,15 @@ public class StockHolderBender : MonoBehaviour
         var tSqr = t * t;
 
         return (p0 * oneMinusTSqr) + (p1 * 2 * oneMinusT * t) + (p2 * tSqr);
+    }
+
+    float RemapRange (float oldValue, float oldMin, float oldMax, float newMin, float newMax)
+    {
+        var oldRange = oldMax - oldMin;
+        var newRange = newMax - newMin;
+
+        var newValue = (((oldValue - oldMin) * newRange) / oldRange) + newMin;
+
+        return newValue;
     }
 }
