1bc7ee03907b9f0425f6a6271649648e050c9229
Marius Stubberud <mars@makingview.com>
2020-08-11 14:35:27 +0200

    Automatic control scheme switching. Messing with teleporter?

---
+++ Assets/Scripts/Game/ControlSchemeManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSchemeManager : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> handObjects;
+    [SerializeField] public List<GameObject> controllerObjects;
+
+    bool prevFrameHandTrackingEnabled = true;
+
+    private void Update()
+    {
+        var handTrackingEnabled = OVRPlugin.GetHandTrackingEnabled();
+
+        if (handTrackingEnabled != prevFrameHandTrackingEnabled)
+        {
+            if (handTrackingEnabled)
+            {
+                foreach (var obj in handObjects)
+                    obj.SetActive(true);
+
+                foreach (var obj in controllerObjects)
+                    obj.SetActive(false);
+            }
+            else
+            {
+                foreach (var obj in handObjects)
+                    obj.SetActive(false);
+
+                foreach (var obj in controllerObjects)
+                    obj.SetActive(true);
+            }
+        }
+
+        prevFrameHandTrackingEnabled = handTrackingEnabled;
+    }
+}
