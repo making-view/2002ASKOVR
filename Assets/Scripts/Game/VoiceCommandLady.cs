e0650f27c823d31d18424d698d6d0cc268d78bd4
Marius Stubberud <mars@makingview.no>
2020-09-16 12:33:48 +0200

    Added voice command lady

---
+++ Assets/Scripts/Game/VoiceCommandLady.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class VoiceCommandLady : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> cShelfNumbers = null;
+    [SerializeField] private List<AudioClip> stockPickAmounts = null;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayCShelfCommand(int shelfNo)
+    {
+        if (shelfNo > 0 && shelfNo <= cShelfNumbers.Count)
+        {
+            audioSource.Stop();
+            audioSource.clip = cShelfNumbers[shelfNo - 1];
+            audioSource.Play();
+        }
+    }
+
+    public void PlayStockPickCommand(int numberOfStock)
+    {
+        if (numberOfStock > 0 && numberOfStock <= stockPickAmounts.Count)
+        {
+            audioSource.Stop();
+            audioSource.clip = stockPickAmounts[numberOfStock - 1];
+            audioSource.Play();
+        }
+    }
+}
