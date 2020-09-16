13d0c11c39b2f53ef05372e63eea4f33c3870ee7
Marius Stubberud <mars@makingview.no>
2020-09-16 17:08:36 +0200

    Keypad is fucking whack broooooooo, but pick list is fucking shit yoooooooo

---
+++ Assets/KeypadButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeypadButton : MonoBehaviour
+{
+    private PushableButton button = null; 
+    private Text text = null;
+    private Keypad keypad = null;
+
+    private void Start()
+    {
+        keypad = FindObjectOfType<Keypad>();
+        button = GetComponentInChildren<PushableButton>();
+        text = GetComponentInChildren<Text>();
+
+        if (text.text.ToLower().Equals("bekreft"))
+            button.onButtonPushed.AddListener(keypad.SendCommand);
+        else if (text.text.ToLower().Equals("gjenta"))
+            button.onButtonPushed.AddListener(keypad.Repeat);
+        else if (text.text.ToLower().Equals("del"))
+            button.onButtonPushed.AddListener(keypad.Backspace);
+        else
+            button.onButtonPushed.AddListener(SendTextToPad);
+    }
+
+    private void SendTextToPad()
+    {
+        keypad.AddToCommand(text.text);
+    }
+}
---
+++ Assets/Scripts/Game/Keypad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] PickList pickList;
+    [SerializeField] Text display;
+
+    private string command = "";
+
+    private void Start()
+    {
+        command = "";
+        display.text = command;
+    }
+
+    public void AddToCommand(string add)
+    {
+        command += add;
+        Debug.Log("Numpad, current command: " + command);
+        display.text = command;
+    }
+
+    public void SendCommand()
+    {
+        Debug.Log("Sending command");
+        pickList.ReceiveCommand(command.Trim());
+        command = "";
+        display.text = command;
+    }
+
+    public void Repeat()
+    {
+        pickList.ReceiveCommand("Repeat");
+    }
+
+    public void Backspace()
+    {
+        command = command.Substring(0, Mathf.Clamp(command.Length - 1, 0, 999));
+        display.text = command;
+    }
+}
