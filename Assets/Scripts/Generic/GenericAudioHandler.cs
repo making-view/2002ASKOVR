80fac3853ee6a6de7f334524541c3c5f3b0b51fc
Marius Stubberud <mars@makingview.com>
2020-10-05 12:44:23 +0200

    Stole Håkon's audiohandler lolololololololo9lololo9lollololool9999999999999999999999

---
+++ Assets/Scripts/Generic/GenericAudioHandler.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Struct containing lists of audioclips
+[System.Serializable]
+public struct ClipList
+{
+    public string name;
+    public List<AudioClip> clip;
+}
+
+public class GenericAudioHandler : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    [Tooltip("Minimum volume in random variation")]
+    public float volumeMin = 0.8f;
+
+    [Range(0.0f, 1.0f)]
+    [Tooltip("Maximum volume in random variation")]
+    public float volumeMax = 1.0f;
+
+    [Range(0.0f, 1.0f)]
+    [Tooltip("Minimum pitch in random variation")]
+    public float pitchMin = 0.8f;
+
+    [Range(0.0f, 2.0f)]
+    [Tooltip("Maximum pitch in random variation")]
+    public float pitchMax = 1.2f;
+
+    [SerializeField]
+    protected string startingClips = "";
+
+    // Named lists of audioclips
+    [SerializeField]
+    public ClipList[] audioClips = null;
+    protected List<AudioClip> activeClips = null;
+    private AudioClip lastPlayed = null;
+
+    // Make sure min variables aren't higher than max variables
+    // set starting clips to the one named in editor
+    private void Start()
+    {
+        if (pitchMin > pitchMax)
+            pitchMin = pitchMax;
+
+        if (volumeMin > volumeMax)
+            volumeMin = volumeMax;
+
+        if (!startingClips.Equals(""))
+            UpdateActiveClips(startingClips);
+    }
+
+    // Get previously played audioclip
+    public AudioClip GetAudioClip()
+    {
+        List<AudioClip> clipsToPlay = new List<AudioClip>(activeClips);
+
+        // Don't play same as last time
+        if (lastPlayed != null && clipsToPlay.Count > 1)
+            clipsToPlay.Remove(lastPlayed);
+
+        // Find new clip to play
+        AudioClip clip = clipsToPlay[Random.Range(0, clipsToPlay.Count - 1)];
+
+        lastPlayed = clip;
+
+        return clip;
+    }
+
+    // Get list with name defined in editor
+    protected void UpdateActiveClips(string nameOfList)
+    {
+        foreach(ClipList c in audioClips)
+        {
+            if (c.name.ToUpper().Equals(nameOfList.ToUpper()))
+            {
+                activeClips = c.clip;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Generic/GenericAudioSource.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericAudioSource : MonoBehaviour
+{
+    
+    protected AudioSource audioSource;
+
+    [SerializeField]
+    [Tooltip("Audio handler containing resources for audiosource to play. Can be in parent or defined here")]
+    protected GenericAudioHandler audioHandler = null;
+
+    // Internal list updated each time it plays audio
+    protected float pitchMin, pitchMax, volumeMin, volumeMax;
+
+    // Find audio handler on start
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        // Try to get audio handler from parent if reference not set in editor
+        if (audioHandler == null)
+            audioHandler = gameObject.GetComponentInParent<GenericAudioHandler>();
+
+        if (audioHandler == null)
+            throw new System.Exception("No audio handler found", null);
+
+        UpdateVariablesFromHandler();
+    }
+
+    // Update variables from audioHandler
+    virtual protected void UpdateVariablesFromHandler()
+    {
+        pitchMax = audioHandler.pitchMax;
+        pitchMin = audioHandler.pitchMin;
+        volumeMax = audioHandler.volumeMax;
+        volumeMin = audioHandler.volumeMin;
+    }
+
+    virtual public void PlaySound()
+    {
+        // Stop if still playing sound
+        if (audioSource.isPlaying)
+            return;
+
+        // Add variations to sound
+        audioSource.pitch = Random.Range(pitchMin, pitchMax);
+        audioSource.volume = Random.Range(volumeMin, volumeMax);
+
+        // Play sound
+        audioSource.clip = audioHandler.GetAudioClip();
+        audioSource.Play();
+    }
+
+
+
+}
