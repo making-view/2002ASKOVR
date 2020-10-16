918f01810c8733f0ea618931cb138ea02fb68989
Marius Stubberud <mars@makingview.com>
2020-10-16 10:37:00 +0200

    Did the sound thang ya kno

--- Assets/Scripts/Generic/GenericAudioSource.cs
+++ Assets/Scripts/Generic/GenericAudioSource.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GenericAudioSource : MonoBehaviour
 {
-    
-    protected AudioSource audioSource;
+
+    protected List<AudioSource> audioSources;
 
     [SerializeField]
     [Tooltip("Audio handler containing resources for audiosource to play. Can be in parent or defined here")]
     protected GenericAudioHandler audioHandler = null;
+    [SerializeField]
+    [Range(1, 5)]
+    protected int maxConcurrentSounds = 2;
 
     // Internal list updated each time it plays audio
     protected float pitchMin, pitchMax, volumeMin, volumeMax;
@@ -17,11 +21,16 @@ public class GenericAudioSource : MonoBehaviour
     // Find audio handler on start
     void Start()
     {
-        audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.playOnAwake = false;
-        audioSource.spatialBlend = 0.5f;
-        audioSource.minDistance = 0.1f;
-        audioSource.maxDistance = 50f;
+        audioSources = new List<AudioSource>();
+
+        for (int i = 0; i < maxConcurrentSounds; ++i)
+        {
+            audioSources.Add(gameObject.AddComponent<AudioSource>());
+            audioSources[i].playOnAwake = false;
+            audioSources[i].spatialBlend = 0.5f;
+            audioSources[i].minDistance = 0.1f;
+            audioSources[i].maxDistance = 50f;
+        }
 
         // Try to get audio handler from parent if reference not set in editor
         if (audioHandler == null)
@@ -44,13 +53,17 @@ public class GenericAudioSource : MonoBehaviour
 
     virtual public void PlaySound()
     {
-        // Add variations to sound
-        audioSource.pitch = Random.Range(pitchMin, pitchMax);
-        audioSource.volume = Random.Range(volumeMin, volumeMax);
+        var availableSource = audioSources.FirstOrDefault(a => !a.isPlaying);
 
-        // Play sound
-        audioSource.Stop();
-        audioSource.clip = audioHandler.GetAudioClip();
-        audioSource.Play();
+        if (availableSource)
+        {
+            // Add variations to sound
+            availableSource.pitch = Random.Range(pitchMin, pitchMax);
+            availableSource.volume = Random.Range(volumeMin, volumeMax);
+
+            // Play sound
+            availableSource.clip = audioHandler.GetAudioClip();
+            availableSource.Play();
+        }
     }
 }
