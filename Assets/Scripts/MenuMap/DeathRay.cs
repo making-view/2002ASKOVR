11912df4c3ebd068bd36ef95fa2162b7e15a400e
Håkon Kleppe Normann <hkn@makingview.com>
2021-01-29 16:42:47 +0100

    Big cow make big head turn. Very cool

--- Assets/Scripts/MenuMap/DeathRay.cs
+++ Assets/Scripts/MenuMap/DeathRay.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class DeathRay : MonoBehaviour
 {
     GameObject target = null;
+    Vector3 startForward;
+    [SerializeField] float maxAngle = 45.0f;
+    [SerializeField] bool debugLog = false;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("FrontMount");
+        startForward = transform.forward;
+        //Debug.Log("start forward: " + startForward.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target.transform);
+
+        var targetVector = (target.transform.position - transform.position).normalized;
+
+        var angle = Vector3.Angle(startForward, targetVector);
+        
+        //if(debugLog)
+        //    Debug.Log("angle b4: " + angle);
+
+        if(angle > maxAngle)
+        {
+            targetVector = Vector3.Lerp(startForward, targetVector, maxAngle / angle);
+            
+            //if (debugLog)
+            //    Debug.Log("angle af: " + Vector3.Angle(startForward, targetVector));
+
+            transform.LookAt(transform.position + targetVector);
+        }
+        else
+            transform.LookAt(target.transform);
     }
 }
