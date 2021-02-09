ce643aca8bef294c2b08f0e0a034ef927893e9c1
Håkon Kleppe Normann <hkn@makingview.com>
2021-02-09 13:23:01 +0100

    Cleanup in videotextures and script names. Sattelite now floats

--- Assets/Scripts/MenuMap/LookAtTruck.cs
+++ Assets/Scripts/MenuMap/LookAtTruck.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathRay : MonoBehaviour
+public class LookAtTruck : MonoBehaviour
 {
     GameObject target = null;
     Vector3 startForward;
